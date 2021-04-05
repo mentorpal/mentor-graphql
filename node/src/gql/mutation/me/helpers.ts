0536dc5430a643280d57a69abaaadfa320f88fab
larry kirschner <larrykirschner@gmail.com>
2021-04-05 14:25:09 -0700

    fixes typescript errors and undoes problem not-null input props (#13)

--- node/src/gql/mutation/me/helpers.ts
+++ node/src/gql/mutation/me/helpers.ts
@@ -7,11 +7,15 @@ The full terms of this copyright and license should always be found in the root 
 import mongoose from 'mongoose';
 
 interface IdAndProps<T> {
-  _id: mongoose.Types.ObjectId;
+  _id: string;
   props: Partial<T>;
 }
 
-export function toUpdateProps<T>(
+interface HasId {
+  _id: string;
+}
+
+export function toUpdateProps<T extends HasId>(
   update: Partial<T>,
   idKeyName = '_id'
 ): IdAndProps<T> {
@@ -20,7 +24,7 @@ export function toUpdateProps<T>(
     props: Object.getOwnPropertyNames(update).reduce(
       (acc: Partial<T>, cur: string) => {
         if (cur !== idKeyName) {
-          acc[cur] = update[cur];
+          acc[cur as keyof T] = update[cur as keyof T];
         }
         return acc;
       },
@@ -32,7 +36,7 @@ export function toUpdateProps<T>(
 // check if id is a valid ObjectID:
 //  - if valid, return it
 //  - if invalid, create a valid object id
-export function idOrNew(id: string) {
+export function idOrNew(id: string): string {
   if (!Boolean(id)) {
     return `${mongoose.Types.ObjectId()}`;
   }
