baf261e9ec6172d43d8a8350e925980bfb78aca5
Kayla Carr <whizbon@gmail.com>
2021-06-24 14:38:25 -0700

    Make request payload size 1mb

--- node/src/app.ts
+++ node/src/app.ts
@@ -27,8 +27,8 @@ export async function createApp(): Promise<Express> {
     app.use(morgan('dev'));
   }
   app.use(cors());
-  app.use(express.json({ limit: '50mb' }));
-  app.use(express.urlencoded({ limit: '50mb' }));
+  app.use(express.json({ limit: '1mb' }));
+  app.use(express.urlencoded({ limit: '1mb' }));
   app.use('/graphql', gqlMiddleware);
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(function (
