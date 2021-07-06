b5f14098f1b750c5be92bde1e73af035f58a20b1
benmai22 <bcmai@cpp.edu>
2021-07-06 12:47:44 -0700

    Secure Cookie Authentication

--- node/src/gql/types/user-access-token.ts
+++ node/src/gql/types/user-access-token.ts
@@ -6,10 +6,12 @@ The full terms of this copyright and license should always be found in the root 
 */
 import { GraphQLString, GraphQLObjectType } from 'graphql';
 import jwt from 'jsonwebtoken';
+import { randomBytes } from 'crypto';
 import { User } from 'models/User';
 import UserType from './user';
 import DateType from './date';
-
+import { Response } from 'express';
+import { RefreshToken as RefreshTokenSchema } from 'models';
 export interface UserAccessToken {
   user: User;
   accessToken: string;
@@ -21,6 +23,75 @@ export function accessTokenDuration(): number {
   return process.env.ACCESS_TOKEN_LENGTH
     ? parseInt(process.env.ACCESS_TOKEN_LENGTH)
     : 60 * 60 * 24 * 90;
+}
+
+export async function getRefreshedToken(token: string) {
+  const refreshToken = await getRefreshToken(token);
+  const { user } = refreshToken;
+
+  // replace old refresh token with a new one and save
+  const newRefreshToken = await generateRefreshToken(user);
+  refreshToken.revoked = new Date(Date.now());
+  refreshToken.replacedByToken = newRefreshToken.token;
+  await refreshToken.save();
+  await newRefreshToken.save();
+
+  // generate new jwt
+  const jwtToken = generateJwtToken(user);
+  return {jwtToken,newRefreshToken, user};
+  // return basic details and tokens
+  // return { 
+  //     ...basicDetails(user),
+  //     jwtToken,
+  //     refreshToken: newRefreshToken.token
+  // };
+}
+
+async function getRefreshToken(token:string) {
+  const refreshToken = await RefreshTokenSchema.findOne({ token }).populate('user');
+  if (!refreshToken || !refreshToken.isActive) throw 'invalid token';
+  return refreshToken;
+}
+
+export function setTokenCookie(res:Response, token: string)
+{
+    // create http only cookie with refresh token that expires in 7 days
+    const cookieOptions = {
+        httpOnly: true,
+        expires: new Date(Date.now() + 7*24*60*60*1000)
+    };
+    res.cookie('refreshToken', token, cookieOptions);
+}
+
+
+function randomTokenString() {
+  return randomBytes(40).toString('hex');
+}
+
+export function generateRefreshToken(user: User) {
+  // create a refresh token that expires in 7 days
+  return new RefreshTokenSchema({
+      user: user.id,
+      token: randomTokenString(),
+      expires: new Date(Date.now() + 7*24*60*60*1000),
+  }).save();
+  // caller needs to call save to save
+}
+
+export function generateJwtToken(user: User) {
+  // generates short lived (15 min) access tokens
+  const expiresIn = 15 * 60; // 15 minute expiry
+  const expirationDate = new Date(Date.now() + expiresIn * 1000);
+  const accessToken = jwt.sign(
+    { id: user._id, expirationDate },
+    process.env.JWT_SECRET,
+    { expiresIn }
+  );
+  return {
+    user,
+    accessToken,
+    expirationDate,
+};
 }
 
 export function generateAccessToken(user: User): UserAccessToken {
