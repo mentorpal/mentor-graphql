0f6a4ae60b2dc7214b9b4f967a7f77c30e8a3119
aaronshiel <ashiel408@gmail.com>
2021-10-04 21:35:48 -0700

    error handling on save

--- node/src/gql/mutation/api/upload-task-status-update.ts
+++ node/src/gql/mutation/api/upload-task-status-update.ts
@@ -54,8 +54,7 @@ export const uploadTaskStatusUpdate = {
       question: args.questionId,
     })
       .then((uploadTask) => {
-        if(!uploadTask)
-          return false;
+        if (!uploadTask) return false;
         const updatedTaskList = uploadTask.taskList;
         const taskIndex = updatedTaskList.findIndex(
           (task) => task.task_id == args.taskId
@@ -72,12 +71,14 @@ export const uploadTaskStatusUpdate = {
           uploadTask.media = args.media;
           uploadTask.markModified('media');
         }
-        uploadTask.save().catch((err)=>{
-          console.log(`failed to save. error: ${err}`)
-        })
+        uploadTask.save().catch((err) => {
+          console.log(`failed to save. error: ${err}`);
+        });
       })
       .catch((err) => {
-        console.log(`no task found for mentor ${mentor._id} and question ${args.questionId}`)
+        console.log(
+          `no task found for mentor ${mentor._id} and question ${args.questionId}`
+        );
         console.log(err);
         return false;
       });
