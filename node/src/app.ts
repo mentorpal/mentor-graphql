72aec77504c148a47dd7636276fb611f6fb389a5
Milan Simonovic <mbsimonovic@gmail.com>
2021-12-11 12:04:53 +0100

    fix warning
    w/o extended option, body-parser complains:
     deprecated undefined extended
    
     http://expressjs.com/en/resources/middleware/body-parser.html

--- node/src/app.ts
+++ node/src/app.ts
@@ -56,7 +56,7 @@ export async function createApp(): Promise<Express> {
   app.use(cors(corsOptions));
   app.use(express.json({ limit: '1mb' }));
   app.use(cookieParser());
-  app.use(express.urlencoded({ limit: '1mb' }));
+  app.use(express.urlencoded({ limit: '1mb', extended: true }));
   // in order to test behind nginx, these two must be under /graphql:
   app.get('/graphql/test-error-handler', (_, res) => {
     res.send(
