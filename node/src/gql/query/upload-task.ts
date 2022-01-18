15d75733626682f47cf70647bd303c13babea545
aaronshiel <ashiel408@gmail.com>
2022-01-18 13:42:33 -0800

    format

--- node/src/gql/query/upload-task.ts
+++ node/src/gql/query/upload-task.ts
@@ -5,11 +5,7 @@ Permission to use, copy, modify, and distribute this software and its documentat
 The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
 
-import {
-  GraphQLID,
-  GraphQLNonNull,
-  GraphQLObjectType,
-} from 'graphql';
+import { GraphQLID, GraphQLNonNull, GraphQLObjectType } from 'graphql';
 import { User, UserRole } from 'models/User';
 import { UploadTask as UploadTaskModel } from 'models';
 import { UploadTask } from 'models/UploadTask';
