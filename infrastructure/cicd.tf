040d0ebfbb9d9f93b0d19d28cb74e25dffc23578
Milan Simonovic <mbsimonovic@gmail.com>
2022-06-10 15:37:01 +0200

    run integration tests after qa deploy

--- infrastructure/cicd.tf
+++ infrastructure/cicd.tf
@@ -1,5 +1,6 @@
 module "pipeline" {
-  source                  = "git@github.com:mentorpal/terraform-modules//modules/trunk_cicd_pipeline?ref=tags/v1.2.2"
+  # source                  = "git@github.com:mentorpal/terraform-modules//modules/trunk_cicd_pipeline?ref=tags/v1.2.2"
+  source                  = "git@github.com:mentorpal/terraform-modules//modules/trunk_cicd_pipeline?ref=cicd-test-stage"
   codestar_connection_arn = var.codestar_connection_arn
   project_name            = "mentor-graphql"
   github_repo_name        = "mentor-graphql"
@@ -11,13 +12,16 @@ module "pipeline" {
   build_buildspec             = "cicd/buildspec.yml"
   deploy_staging_buildspec    = "cicd/deployspec_staging.yml"
   deploy_prod_buildspec       = "cicd/deployspec_prod.yml"
+  
+  e2e_tests_buildspec         = "cicd/e2espec.yml"
   deploys_privileged_mode     = false
   export_pipeline_info        = true
   enable_status_notifications = true
 
   tags = {
-    Source  = "terraform"
-    Project = "mentorpal"
+    Source    = "terraform"
+    Project   = "mentorpal"
+    Component = "graphql"
   }
 
   providers = {
